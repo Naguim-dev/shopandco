7632f4a2ff6a0192889808a66bcc7b1bc6090b38
Naguim Benabdellah <naguimb@gmail.com>
2022-07-18 19:55:41 +0200

    Verification user's account is activated + validation flash message

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -61,7 +61,7 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_home'));
+        return new RedirectResponse($this->urlGenerator->generate('home'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
